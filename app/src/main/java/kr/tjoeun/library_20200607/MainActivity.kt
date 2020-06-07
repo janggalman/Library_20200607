9069951994e6fbb6b90d9e965ebe01457668270a
janggalman <janggalman@gmail.com>
2020-06-07 11:10:13 +0900

    프시를 누르면 프사 크게 보기  => 줌인/줌아웃 구현 필요  => 더블클릭시 줌인도 구현 필요

--- app/src/main/java/kr/tjoeun/library_20200607/MainActivity.kt
+++ app/src/main/java/kr/tjoeun/library_20200607/MainActivity.kt
@@ -1,7 +1,10 @@
 package kr.tjoeun.library_20200607
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -13,6 +16,14 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvent() {
+
+        profileImg.setOnClickListener{
+            Log.d("메인화면" , "이미지 뷰 클릭")
+            val myIntent = Intent(mContext, ViewProfileImageActivity::class.java)
+            startActivity(myIntent)
+        }
+
+
 
     }
 
---
+++ app/src/main/java/kr/tjoeun/library_20200607/ViewProfileImageActivity.kt
@@ -0,0 +1,20 @@
+package kr.tjoeun.library_20200607
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfileImageActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile_image)
+    }
+
+    override fun setupEvent() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
