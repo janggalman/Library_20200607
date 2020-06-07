b5713c98f2442439abb623645cafe1f079404b9e
janggalman <janggalman@gmail.com>
2020-06-07 13:46:44 +0900

    TedPermission 라이브러리로 권한 체크 후 통화 실행

--- app/src/main/java/kr/tjoeun/library_20200607/MainActivity.kt
+++ app/src/main/java/kr/tjoeun/library_20200607/MainActivity.kt
@@ -1,11 +1,16 @@
 package kr.tjoeun.library_20200607
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : BaseActivity() {
 
@@ -22,9 +27,35 @@ class MainActivity : BaseActivity() {
             Log.d("메인화면" , "이미지 뷰 클릭")
             val myIntent = Intent(mContext, ViewProfileImageActivity::class.java)
             startActivity(myIntent)
+
+
         }
 
+        callBtn.setOnClickListener {
 
+//                전화를 걸기 전에 CALL 권한을 얻었는지 체크.
+//                권한을 허가 받은 경우에만 => 실제로 전화
+
+            val permissionListener = object : PermissionListener {
+                override fun onPermissionGranted() {
+//                        허가가 난 경우
+                    //               실제 전화를 거는 코드
+                    val myUri = Uri.parse("tel:${phoneNumTxt.text}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext , "권한이 거부되어 통화를 할 수 없습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            TedPermission.with(mContext)
+                .setPermissionListener(permissionListener)
+                .setDeniedMessage("거절되면 통화할 수 없으니 [설정]에서 권한을 허가해주세요.")
+                .setPermissions(android.Manifest.permission.CALL_PHONE)
+                .check()
+        }
 
     }
 
